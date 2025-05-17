dc77b2699c33892d450222e42b4f6e3db1ed6aef
Julia lopes <jlr.nwb@gmail.com>
2025-05-17 14:49:05 -0300

    feat: setting up kafka publisher

--- publisher/src/main/java/com/br/publisher/controller/RabbitMQController.java
+++ publisher/src/main/java/com/br/publisher/controller/RabbitMQController.java
@@ -2,11 +2,8 @@ package com.br.rabbitmq_publisher.controller;
 
 import com.br.rabbitmq_publisher.model.MessageRequest;
 import io.micrometer.core.instrument.Counter;
-import lombok.RequiredArgsConstructor;
 import org.springframework.amqp.rabbit.core.RabbitTemplate;
-import org.springframework.beans.factory.annotation.Qualifier;
 import org.springframework.beans.factory.annotation.Value;
-import org.springframework.context.MessageSource;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
@@ -15,23 +12,21 @@ import org.springframework.web.bind.annotation.RestController;
 import io.micrometer.core.instrument.MeterRegistry;
 
 @RestController
-public class MessageController {
+public class RabbitMQController {
 
     private final RabbitTemplate rabbitTemplate;
     private final String queueName;
     private final Counter messageCounter;
-    private final MessageSource messageSource;
 
-    public MessageController(RabbitTemplate rabbitTemplate,
-                             @Value("${app.queue.name}") String queueName,
-                             MeterRegistry registry, @Qualifier("messageSource") MessageSource messageSource) {
+    public RabbitMQController(RabbitTemplate rabbitTemplate,
+                              @Value("${app.queue.name}") String queueName,
+                              MeterRegistry registry) {
         this.rabbitTemplate = rabbitTemplate;
         this.queueName = queueName;
         this.messageCounter = registry.counter("messages.sent.total");
-        this.messageSource = messageSource;
     }
 
-    @PostMapping("/message")
+    @PostMapping("/rabbitmq/message")
     public ResponseEntity<Void> publish(@RequestBody String message) {
         MessageRequest rabbitmqMessage = MessageRequest.builder()
                 .content(message)
@@ -41,4 +36,6 @@ public class MessageController {
         messageCounter.increment();
         return ResponseEntity.ok().build();
     }
+
+
 }
